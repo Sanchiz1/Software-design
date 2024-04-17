f74bf08b22846443876ee832065d1c1501e81c42
Sasha Zaitsev <sashazaitsev2005@gmail.com>
2024-04-17 21:10:19 +0300

    Iterator

---
+++ lab-03/StructuralPatterns/Composite/Iterator/LightNodeAggregate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite.Iterator;
+public class LightNodeAggregate : IEnumerable<ILightNode>
+{
+    public IterationType _type;
+    public List<ILightNode> LightNodes;
+
+    public LightNodeAggregate(List<ILightNode> lightNodes, IterationType type)
+    {
+        LightNodes = lightNodes;
+        _type = type;
+    }
+
+    public IEnumerator<ILightNode> GetEnumerator()
+    {
+        if (_type is IterationType.Breadth)
+        {
+            return new LightNodeBreadthIterator(this);
+        }
+        else if (_type is IterationType.Depth)
+        {
+            return new LightNodeDepthIterator(this);
+        }
+
+        throw new ArgumentException("Unsupported iteration type");
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public enum IterationType
+    {
+        Breadth,
+        Depth
+    }
+}
